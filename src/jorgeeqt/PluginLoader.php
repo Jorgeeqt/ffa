17e5a7a7a910b9f31dd48a7aabb278245a667a9c
Jorgeeqt <104584243+Jorgeeqt@users.noreply.github.com>
2022-06-26 11:12:38 -0500

    Update PluginLoader.php

--- src/jorgeeqt/PluginLoader.php
+++ src/jorgeeqt/PluginLoader.php
@@ -2,6 +2,12 @@
 
 namespace jorgeeqt;
 
+use jorgeeqt\PluginInstanceReference;
+use jorgeeqt\util\{Utils, Permissions};
+use jorgeeqt\arena\{Arena, ArenaManager};
+use jorgeeqt\provider\{SessionManager, Session};
+use jorgeeqt\task\{RespawnTask, ScoreboardTask};
+use jorgeeqt\kits\types\{NoDebuff, Combo, Gapple};
 use pocketmine\Server;
 use pocketmine\plugin\PluginBase;
 use pocketmine\world\World;
@@ -9,14 +15,33 @@ use pocketmine\world\World;
 class PluginLoader extends PluginBase {
   
   private static ?PluginLoader $instance;
+  private Utils $utils;
+  private ArenaManager $arenaManager;
+  private Types $kitManager;
   
   public function onEnable(): void 
   {
     $this->getLogger()->info();
   }
   
-  public static function getInstance(){
+  public static function getInstance()
+  {
     return self::$instance;
+  }
+  
+  public function getUtils()
+  {
+    return $this->utils;
+  }
+  
+  public function getKits()
+  {
+    return $this->kitManager;
+  }
+  
+  public function getArenaManager()
+  {
+    return $this->arenaManager;
   }
 }
 <?
